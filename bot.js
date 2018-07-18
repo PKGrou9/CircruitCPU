3dd85c27731fef3e2ed952a0b1a4f351c5db3bf5
PKGrou9 <41385472+PKGrou9@users.noreply.github.com>
2018-07-18 19:05:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '..') {
+    	message.reply('My Prefix');
   	}
 });
 
