33abc9c8b284588672b7954a892b928495b434c2
Anna <41385472+PKGrou9@users.noreply.github.com>
2018-07-19 19:55:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,8 +25,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '..donate') {
-    	message.reply('if you Wanna Donate to Help My Programmation : PayPal [ pkgrou9@gmail.com ] Donate to this E-Mail / Invite Me
-                      https://discordapp.com/api/oauth2/authorize?client_id=461174799599927297&permissions=0&scope=bot');
+    	message.reply('if you Wanna Donate to Help My Programmation : PayPal [ pkgrou9@gmail.com ] Donate to this E-Mail / Invite Me https://discordapp.com/api/oauth2/authorize?client_id=461174799599927297&permissions=0&scope=bot');
   	}  
 });
 
