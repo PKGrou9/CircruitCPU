b6b6fc1e58af75ffe060726256454da3e70f82c9
FSxOmega <41387833+FSxOmega@users.noreply.github.com>
2018-07-19 14:53:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,27 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '..help') {
-    	message.reply('PONG - My Commands are:
-                      ..patches
-                      ..yt
-                      More will come i a few Updates!
-                      Bot Creator:Windows97
-                      Bot Co-Creator:FSxOmega');
-  	}
-});
-
-client.on('message', message => {
     if (message.content === '..patches') {
     	message.reply('PONG - None found in Cloud');
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '..yt') {
-    	message.reply('PONG - https://m.youtube.com/channel/UCf2BRnCkA7do5wvKgwtAAhg');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
