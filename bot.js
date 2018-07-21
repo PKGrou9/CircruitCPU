1c75f34048fc978faf2ddeece0b2fa3f3b2f6ad6
Anna <41385472+PKGrou9@users.noreply.github.com>
2018-07-21 10:52:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,84 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//This is the Bot-Prefix
+const prefix = '..';
 
-//If the bot is Ready (in Console)
+//Bot on Ready
 client.on('ready', () => {
-    console.log('Booted...');
+    console.log('Bot Launched!:D');
 });
 
+//Command +info
 client.on('message', message => {
-    if (message.content === '..patches') {
+    if (message.content === prefix + 'info') {
+
         message.channel.send({embed: {
-        color: 0xff0000,
-        description : "PONG - New Prefix [..] / New CMD - ..donate / donate and help my programmation !"
-  	}});
+  color: 3447003,
+ 
+  description: "Hey Its Me CircruitCPU - FSxAlpha's Brother"
+}});
+      }  
 });
 
+//Command +help
 client.on('message', message => {
-    if (message.content === '..help') {
+    if (message.content === prefix + 'help') {
+
         message.channel.send({embed: {
-        color: 0xff0000,
-        description: "Ok Sure Here - CMDs : / ..help / ..donate / ..yt / ..patches / Other : / Nan Err - EExx /"
-  	}});
+  color: 3447003,
+  
+  description: "CMDs : ..help / ..yt / ..invite / ..donate / ..support"
+
+}});
+      }  
 });
 
+//Command +yt
 client.on('message', message => {
-    if (message.content === '..yt') {
+    if (message.content === prefix + 'yt') {
+
         message.channel.send({embed: {
-        color: 0xff0000,
-        description: "YT Official :  https://m.youtube.com/channel/UCTJQkBj3dhkgA1TXnpZGkhw"
-  	}});
+  color: 3447003,
+ 
+  description: "YouTube : https://m.youtube.com/channel/UCTJQkBj3dhkgA1TXnpZGkhw"
+}});
+      }  
 });
 
+//Command +invite
 client.on('message', message => {
-    if (message.content === '..donate') {
+    if (message.content === prefix + 'invite') {
         message.channel.send({embed: {
-        color: 0xff0000,
-    	description: "If you Wanna Donate to Help My Programmation : PayPal [ pkgrou9@gmail.com ] Donate to this E-Mail!"
-  	}});
+  color: 3447003,
+
+  description: "Wanna invite me to your Server? - https://discordapp.com/oauth2/authorize?client_id=469517178757185546&permissions=8&scope=bot"
+}});
+
+      }  
 });
 
-// THIS  MUST  BE  THIS  WAY
+//Command +donate
+client.on('message', message => {
+    if (message.content === prefix + 'donate') {
+
+        message.channel.send({embed: {
+  color: 3447003,
+  
+  description: "If you wanna Donate for faster Coding - Donate to This PayPal [ pkgrou9@gmail.com]"
+}});
+      }
+});
+
+//Command +support
+client.on('message', message => {
+    if (message.content === prefix + 'support') {
+
+        message.channel.send({embed: {
+  color: 3447003,
+ 
+  description: "Wanna join my Support Discord Server? - https://discord.gg/GnUczTb'"
+}});
+      }
+});
+
 client.login(process.env.BOT_TOKEN);
